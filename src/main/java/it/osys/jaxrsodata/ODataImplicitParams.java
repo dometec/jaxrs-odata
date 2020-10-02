8fd63451869c01cd4155ce89d56134873d3bc8c6
Domenico Briganti <domenico.briganti@kalpa.it>
2020-10-02 12:35:28 +0200

    Add search to swagger definition

--- src/main/java/it/osys/jaxrsodata/ODataImplicitParams.java
+++ src/main/java/it/osys/jaxrsodata/ODataImplicitParams.java
@@ -9,11 +9,12 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 
 @ApiImplicitParams(value = {
-        @ApiImplicitParam(paramType = "query", name = "$top", dataType = "int", value = "OData $top parameter"),
-        @ApiImplicitParam(paramType = "query", name = "$skip", dataType = "int", value = "OData $skip parameter"),
-        @ApiImplicitParam(paramType = "query", name = "$count", dataType = "boolean", value = "OData $count parameter"),
-        @ApiImplicitParam(paramType = "query", name = "$orderby", dataType = "string", value = "OData $orderby parameter", example = "id asc"),
-        @ApiImplicitParam(paramType = "query", name = "$filter", dataType = "string", value = "OData $filter parameter", example = "id eq 1")
+        @ApiImplicitParam(paramType = "query", name = "$top", dataType = "int", value = "Max number item to get"),
+        @ApiImplicitParam(paramType = "query", name = "$skip", dataType = "int", value = "How many record skip before the first item"),
+        @ApiImplicitParam(paramType = "query", name = "$count", dataType = "boolean", value = "True to get also the total number of item the server has"),
+        @ApiImplicitParam(paramType = "query", name = "$search", dataType = "string", value = "String to search in items"),
+        @ApiImplicitParam(paramType = "query", name = "$orderby", dataType = "string", value = "Order by the result", example = "id asc"),
+        @ApiImplicitParam(paramType = "query", name = "$filter", dataType = "string", value = "Filter the result", example = "id eq 1")
 })
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
